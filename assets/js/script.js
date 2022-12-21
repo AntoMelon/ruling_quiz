0f75755b0af4698a13759f61e83ada6016f20362
AntoMelon <82177158+AntoMelon@users.noreply.github.com>
2022-12-22 00:54:13 +0100

    Added first functions to generate quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,29 @@
 "use strict;"
 
-var questions = [];
+var ruling_questions = null;
 
-console.log("test");
-console.log(fetch('./assets/json/questions/questions.json').then(response => response.json()));
-
-fetch('./assets/json/questions/questions.json').then(response => response.json()).then(function (data) {
-    console.log(data);
+fetch('./assets/json/questions/ruling_questions.json').then(response => response.json()).then(function (data) {
+    ruling_questions = data;
 })
+
+function areQuestionsLoaded() {
+    return ruling_questions !== null;
+}
+
+
+//Durstenfeld shuffle
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function generateQuizOfSize(questions,size) {
+    shuffleArray(questions);
+    return questions.slice(0,size);
+}
+
+document.addEventListener("DOMContentLoaded",function(e) {
+
+});
